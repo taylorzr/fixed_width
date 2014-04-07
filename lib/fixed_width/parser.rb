c51bfdde3182897aeaff5d00917a4c8b173ef003
Colin Curtin <colin.t.curtin@gmail.com>
2014-04-07 13:58:56 -0700

    Optionally parse section by section.

--- lib/fixed_width/parser.rb
+++ lib/fixed_width/parser.rb
@@ -5,15 +5,34 @@ class FixedWidth
       @file       = file
     end
 
-    def parse
+    # Also by_section=false means try to match each line in the file with a section
+    def parse(by_section=true)
       @parsed = {}
-      @content = read_file
-      unless @content.empty?
+      @lines = read_file
+
+      return @parsed if @lines.empty?
+
+      if by_section
         @definition.sections.each do |section|
           rows = fill_content(section)
           raise FixedWidth::RequiredSectionNotFoundError.new("Required section '#{section.name}' was not found.") unless rows > 0 || section.optional
         end
+      else
+        failed = @lines.map.with_index do |line, index|
+          if section = @definition.sections.detect{|s| s.match(line)}
+            add_to_section(section, line)
+            [section, index]
+          else
+            [nil, index]
+          end
+        end.reject{|section, index| section}.map do |_, index|
+          "couldn't parse line #{index + 1}"
+        end
+        if failed.any?
+          raise "Unparsable file: #{failed.join(', ')}"
+        end
       end
+
       @parsed
     end
 
@@ -26,11 +45,11 @@ class FixedWidth
     def fill_content(section)
       matches = 0
       loop do
-        line = @content.first
+        line = @lines.first
         break unless section.match(line)
         add_to_section(section, line)
         matches += 1
-        @content.shift
+        @lines.shift
       end
       matches
     end
