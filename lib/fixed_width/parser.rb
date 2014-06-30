1f93060dcee38026b778fe7efb13a468e75b1d42
Colin Curtin <colin.t.curtin@gmail.com>
2014-06-30 15:29:18 -0700

    default: don't raise on non-parse, just save

--- lib/fixed_width/parser.rb
+++ lib/fixed_width/parser.rb
@@ -5,32 +5,57 @@ class FixedWidth
       @file       = file
     end
 
-    # Also by_section=false means try to match each line in the file with a section
     def parse(by_section=true)
+      if by_section
+        parse_by_section
+      else
+        parse_by_line
+      end
+    end
+
+    # Try to match each line with a section
+    def parse_by_line(raise_on_failure=false, save_unparsable=true)
       @parsed = {}
       @lines = read_file
 
       return @parsed if @lines.empty?
 
-      if by_section
-        @definition.sections.each do |section|
-          rows = fill_content(section)
-          raise FixedWidth::RequiredSectionNotFoundError.new("Required section '#{section.name}' was not found.") unless rows > 0 || section.optional
+      result = @lines.map.with_index do |line, index|
+        if section = @definition.sections.detect{|s| s.match(line)}
+          add_to_section(section, line)
+          [section, line, index]
+        else
+          [nil, line, index]
         end
-      else
-        failed = @lines.map.with_index do |line, index|
-          if section = @definition.sections.detect{|s| s.match(line)}
-            add_to_section(section, line)
-            [section, index]
-          else
-            [nil, index]
-          end
-        end.reject{|section, index| section}.map do |_, index|
-          "couldn't parse line #{index + 1}"
+      end
+
+      @failed = result.reject{|section, _, _| section}
+
+      if raise_on_failure && @failed.any?
+        messages = @failed.map do |_, line, index|
+          "couldn't parse line #{index + 1}: #{line.inspect}"
         end
-        if failed.any?
-          raise "Unparsable file: #{failed.join(', ')}"
-        end
+
+        raise "Unparsable file: #{messages.join(', ')}"
+      end
+
+      if save_unparsable && @failed.any?
+        @parsed[:__unparsable] = @failed.map{|_, line, index| [index, line]}
+      end
+
+      @parsed
+    end
+
+    # Try to fulfill all sections
+    def parse_by_section
+      @parsed = {}
+      @lines = read_file
+
+      return @parsed if @lines.empty?
+
+      @definition.sections.each do |section|
+        rows = fill_content(section)
+        raise FixedWidth::RequiredSectionNotFoundError.new("Required section '#{section.name}' was not found.") unless rows > 0 || section.optional
       end
 
       @parsed
