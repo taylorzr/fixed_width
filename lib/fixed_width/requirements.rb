f6563232a46ecfd18d6e5d507bfa2e5b1a50867f
David Feldman <david.feldman@avantcredit.com>
2014-07-28 16:57:28 -0500

    ActiveSupport 3 or greater is a required dependency now

--- lib/fixed_width/requirements.rb
+++ lib/fixed_width/requirements.rb
@@ -1,10 +1,8 @@
 require 'ostruct'
 
-require 'active_support'
-require 'active_support/version'
 require 'active_support/core_ext/object/try'
 require 'active_support/core_ext/object/blank'
-require 'active_support/multibyte' if ::ActiveSupport::VERSION::MAJOR >= 3
+require 'active_support/multibyte'
 
 require 'fixed_width/core_ext/symbol'
 require 'fixed_width/version'
