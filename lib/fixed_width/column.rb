8dd2a9fffc05d1b6ee7eceee6dbf508fe6659f64
David Feldman <david.feldman@avantcredit.com>
2014-07-29 15:19:58 -0500

    cleanup column options

--- lib/fixed_width/column.rb
+++ lib/fixed_width/column.rb
@@ -1,88 +1,90 @@
 module FixedWidth
   class Column
-    DEFAULT_PADDING   = ' '
-    DEFAULT_ALIGNMENT = :right
-    DEFAULT_TRUNCATE  = false
-    DEFAULT_FORMATTER = :to_s
 
-    attr_reader :name, :length, :alignment, :padding, :truncate, :group
+    DEFAULT_OPTIONS = {
+      alignment: :right,
+      padding: ' ',
+      truncate: false,
+      formatter: :to_s,
+      nil_blank: false,
+      parser: nil,
+      group: nil
+    }.freeze
+
+    attr_reader :name, :length
+
+    [:alignment, :padding, :truncate, :group].each do |m|
+      define_method(m) { opt(m) }
+    end
 
     def initialize(name, length, options={})
-      assert_valid_options(options)
-      @name      = name
-      @length    = length
-      @alignment = options[:align]    || DEFAULT_ALIGNMENT
-      @padding   = options[:padding]  || DEFAULT_PADDING
-      @truncate  = options[:truncate] || DEFAULT_TRUNCATE
-
-      @group     = options[:group]
-
-      @parser    = options[:parser]
-      @parser    = @parser.to_proc if @parser.is_a?(Symbol)
-
-      @formatter = options[:formatter]
-      @formatter ||= DEFAULT_FORMATTER
-      @formatter = @formatter.to_proc if @formatter.is_a?(Symbol)
-
-      @nil_blank = options[:nil_blank]
+      @options = assert_valid_options(options)
+      @name    = name
+      @length  = length
     end
 
     def parse(value, section)
-      if @nil_blank && blank?(value)
-        return nil
-      elsif @parser
-        @parser.call(value)
-      else
-        case @alignment
-        when :right
-          value.lstrip
-        when :left
-          value.rstrip
-        end
+      return nil if opt(:nil_blank) && value.blank?
+      return opt(:parser).call(value) if opt(:parser)
+      case alignment
+      when :right
+        value.lstrip
+      when :left
+        value.rstrip
       end
     rescue
       raise FixedWidth::ParseError.new %{
-        #{section.name}::#{name}: The value '#{value}' could not be parsed: #{$!}
+        #{section.name}::#{name}:
+        The value '#{value}' could not be parsed:
+        #{$!}
       }.squish
     end
 
     def format(value)
       pad(
         validate_size(
-          @formatter.call(value)
+          opt(:formatter).call(value)
         )
       )
     end
 
-    private
-    BLANK_REGEX = /^\s*$/
-    def blank?(value)
-      value =~ BLANK_REGEX
+    def opt(key)
+      @options[key]
     end
 
+    private
+
     def pad(value)
-      case @alignment
+      case alignment
       when :left
-        value.ljust(@length, @padding)
+        value.ljust(length, padding)
       when :right
-        value.rjust(@length, @padding)
+        value.rjust(length, padding)
       end
     end
 
     def assert_valid_options(options)
-      unless options[:align].nil? || [:left, :right].include?(options[:align])
+      opts = DEFAULT_OPTIONS.merge(options)
+      unless [nil, :left, :right].include?(opts[:align])
         raise ArgumentError.new("Option :align only accepts :right (default) or :left")
       end
+      [:parser, :formatter].each do |pkey|
+        opts[pkey] = opts[pkey].to_proc if opts[pkey].respond_to?(:to_proc)
+      end
+      opts
     end
 
     def validate_size(result)
-      return result if result.length <= @length
-      raise FixedWidth::FormattedStringExceedsLengthError.new(
-        "The formatted value '#{result}' in column '#{@name}' exceeds the allowed length of #{@length} chararacters.") unless @truncate
-      case @alignment
-      when :right then result[-@length,@length]
-      when :left  then result[0,@length]
+      return result if result.length <= length
+      raise FixedWidth::FormattedStringExceedsLengthError.new %{
+        The formatted value '#{result}' in column '#{name}'
+        exceeds the allowed length of #{length} chararacters.
+      }.squish unless truncate
+      case alignment
+      when :right then result[-length,length]
+      when :left  then result[0,length]
       end
     end
+
   end
 end
